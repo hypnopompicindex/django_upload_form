17753313c7bc252bb23ac8ad712e067f985b24c5
hypnopompicindex <hypnopompicindex@gmail.com>
2014-03-23 15:38:33 -0400

    added view for upload_form

--- upload_form/views.py
+++ upload_form/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+def create(request):
+    if request.POST:
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            a = form.save()
+
+            messages.add_message(request, messages.SUCCESS, "You Upload was added")
+
+            return HttpResponseRedirect('/upload_form/all')
+    else:
+        form = UploadForm()
+
+    args = {}
+    args.update(csrf(request))
+
+    args['form'] = form
+
+    return render_to_response('create_upload_form.html', args)
