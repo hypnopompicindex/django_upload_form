2e1e112ebf09a5c787ca530a4819720f7e6227c8
hypnopompicindex <hypnopompicindex@gmail.com>
2014-03-23 15:47:06 -0400

    added UploadForm to forms

--- upload_form/forms.py
+++ upload_form/forms.py
@@ -1,1 +1,8 @@
-__author__ = 'tubbyink'
+from django import forms
+from models import Upload
+
+class UploadForm(forms.ModelForm):
+
+    class Meta:
+        model = Upload
+        fields = ('title', 'body', 'pub_date')
