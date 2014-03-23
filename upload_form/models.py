c6fc8752319ac2f3ae8367ff40eac3e5e55502ba
hypnopompicindex <hypnopompicindex@gmail.com>
2014-03-23 15:44:32 -0400

    added get_upload_file_name to model

--- upload_form/models.py
+++ upload_form/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+
+def get_upload_file_name(instance, filename):
+    return settings.UPLOAD_FILE_PATTERN % (str(time()).replace('.','_'), filename)
+
+
 class Upload(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
