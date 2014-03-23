2124fd7f709fdbacebb21d32606aa2d2445878d7
hypnopompicindex <hypnopompicindex@gmail.com>
2014-03-23 15:42:55 -0400

    added Upload model

--- upload_form/models.py
+++ upload_form/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Upload(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "/upload_form/get/%i/" % self.id
